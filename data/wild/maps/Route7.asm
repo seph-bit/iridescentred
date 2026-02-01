e75dd0268239cef5c9e47bcf9318d9caf62afd7e
seph-bit <seraphim.beatle@gmail.com>
2026-02-01 16:03:34 -0600

    Update Route7.asm

--- data/wild/maps/Route7.asm
+++ data/wild/maps/Route7.asm
@@ -1,28 +1,15 @@
 Route7WildMons:
 	def_grass_wildmons 15 ; encounter rate
-	db 19, PIDGEY
-IF DEF(_RED)
-	db 19, ODDISH
-	db 17, MANKEY
-	db 22, ODDISH
-	db 22, PIDGEY
-	db 18, MANKEY
-	db 18, GROWLITHE
-	db 20, GROWLITHE
-	db 19, MANKEY
-	db 20, MANKEY
-ENDC
-IF DEF(_BLUE)
-	db 19, BELLSPROUT
-	db 17, MEOWTH
-	db 22, BELLSPROUT
-	db 22, PIDGEY
-	db 18, MEOWTH
-	db 18, VULPIX
-	db 20, VULPIX
-	db 19, MEOWTH
-	db 20, MEOWTH
-ENDC
+	db 24, DODUO
+	db 25, VULPIX
+	db 24, PRIMEAPE
+	db 25, DODUO
+	db 27, DODUO
+	db 26, PRIMEAPE
+	db 27, VULPIX
+	db 23, PONYTA
+	db 26, PONYTA
+	db 28, PONYTA
 	end_grass_wildmons
 
 	def_water_wildmons 0 ; encounter rate
