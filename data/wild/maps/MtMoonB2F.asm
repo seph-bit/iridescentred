a2670dfb28d48c1e2576a0f71463deea88d6c7c9
seph-bit <seraphim.beatle@gmail.com>
2026-02-01 14:39:36 -0600

    Update MtMoonB2F.asm

--- data/wild/maps/MtMoonB2F.asm
+++ data/wild/maps/MtMoonB2F.asm
@@ -1,15 +1,15 @@
 MtMoonB2FWildMons:
 	def_grass_wildmons 10 ; encounter rate
-	db  9, ZUBAT
-	db  9, GEODUDE
 	db 10, ZUBAT
-	db 10, GEODUDE
-	db 11, ZUBAT
-	db 10, PARAS
-	db 12, PARAS
-	db 10, CLEFAIRY
+	db 14, GEODUDE
 	db 12, ZUBAT
+	db 12, SANDSHREW
+	db 13, ZUBAT
+	db 11, MACHOP
+	db 14, MACHOP
+	db 11, CLEFAIRY
 	db 12, CLEFAIRY
+	db 15, CLEFAIRY
 	end_grass_wildmons
 
 	def_water_wildmons 0 ; encounter rate
