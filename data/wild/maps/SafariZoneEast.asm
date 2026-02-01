2238092fdb5413bd92f8f7aae2af29f480220b2f
seph-bit <seraphim.beatle@gmail.com>
2026-02-01 16:38:26 -0600

    Update SafariZoneEast.asm

--- data/wild/maps/SafariZoneEast.asm
+++ data/wild/maps/SafariZoneEast.asm
@@ -1,29 +1,16 @@
 SafariZoneEastWildMons:
 	def_grass_wildmons 30 ; encounter rate
 IF DEF(_RED)
-	db 24, NIDORAN_M
-	db 26, DODUO
-	db 22, PARAS
-	db 25, EXEGGCUTE
-	db 33, NIDORINO
-	db 23, EXEGGCUTE
-	db 24, NIDORAN_F
-	db 25, PARASECT
-	db 25, KANGASKHAN
-	db 28, SCYTHER
-ENDC
-IF DEF(_BLUE)
-	db 24, NIDORAN_F
-	db 26, DODUO
-	db 22, PARAS
-	db 25, EXEGGCUTE
-	db 33, NIDORINA
-	db 23, EXEGGCUTE
-	db 24, NIDORAN_M
-	db 25, PARASECT
-	db 25, KANGASKHAN
-	db 28, PINSIR
-ENDC
+	db 34, GOLEM
+	db 33, WIGGLYTUFF
+	db 34, MACHAMP
+	db 34, GENGAR
+	db 33, ELECTABUZZ
+	db 33, MAGMAR
+	db 34, NIDOQUEEN
+	db 35, CLEFABLE
+	db 35, NIDOKING
+	db 35, ALAKAZAM
 	end_grass_wildmons
 
 	def_water_wildmons 0 ; encounter rate
