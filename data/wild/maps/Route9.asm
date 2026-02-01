97d6aa7e862748a9534591436b3c3dc49d5f9bee
seph-bit <seraphim.beatle@gmail.com>
2026-02-01 15:52:11 -0600

    Update Route9.asm

--- data/wild/maps/Route9.asm
+++ data/wild/maps/Route9.asm
@@ -1,26 +1,15 @@
 Route9WildMons:
 	def_grass_wildmons 15 ; encounter rate
-	db 16, RATTATA
-	db 16, SPEAROW
-	db 14, RATTATA
-IF DEF(_RED)
-	db 11, EKANS
-	db 13, SPEAROW
-	db 15, EKANS
-	db 17, RATTATA
-	db 17, SPEAROW
-	db 13, EKANS
-	db 17, EKANS
-ENDC
-IF DEF(_BLUE)
-	db 11, SANDSHREW
-	db 13, SPEAROW
-	db 15, SANDSHREW
-	db 17, RATTATA
-	db 17, SPEAROW
-	db 13, SANDSHREW
-	db 17, SANDSHREW
-ENDC
+	db 19, NIDORAN_M
+	db 20, FEAROW
+	db 21, NIDORINO
+	db 19, NIDORAN_F
+	db 21, FEAROW
+	db 22, RATICATE
+	db 23, FEAROW
+	db 24, NIDORINO
+	db 22, NIDORINA
+	db 24, NIDORINA
 	end_grass_wildmons
 
 	def_water_wildmons 0 ; encounter rate
