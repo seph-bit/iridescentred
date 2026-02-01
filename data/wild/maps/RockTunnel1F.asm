bd7ae526942aa6e68876e585c525215e164418d6
seph-bit <seraphim.beatle@gmail.com>
2026-02-01 15:56:41 -0600

    Update RockTunnel1F.asm

--- data/wild/maps/RockTunnel1F.asm
+++ data/wild/maps/RockTunnel1F.asm
@@ -1,15 +1,15 @@
 RockTunnel1FWildMons:
 	def_grass_wildmons 15 ; encounter rate
-	db 16, ZUBAT
-	db 17, ZUBAT
-	db 17, GEODUDE
-	db 15, MACHOP
-	db 16, GEODUDE
-	db 18, ZUBAT
-	db 15, ZUBAT
-	db 17, MACHOP
-	db 13, ONIX
-	db 15, ONIX
+	db 19, ZUBAT
+	db 21, GEODUDE
+	db 21, GOLBAT
+	db 22, MACHOP
+	db 23, GEODUDE
+	db 21, ZUBAT
+	db 22, SANDSLASH
+	db 25, ONIX
+	db 23, GOLBAT
+	db 24, GOLBAT
 	end_grass_wildmons
 
 	def_water_wildmons 0 ; encounter rate
