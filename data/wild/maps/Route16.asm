a8843f40607fdf6b78bcae0f0c038adf26e2139b
seph-bit <seraphim.beatle@gmail.com>
2026-02-01 16:08:20 -0600

    Update Route16.asm

--- data/wild/maps/Route16.asm
+++ data/wild/maps/Route16.asm
@@ -1,15 +1,15 @@
 Route16WildMons:
 	def_grass_wildmons 25 ; encounter rate
-	db 20, SPEAROW
-	db 22, SPEAROW
-	db 18, RATTATA
-	db 20, DODUO
-	db 20, RATTATA
-	db 18, DODUO
-	db 22, DODUO
-	db 22, RATTATA
-	db 23, RATICATE
+	db 24, GRIMER
 	db 25, RATICATE
+	db 25, GRIMER
+	db 24, DODUO
+	db 26, RATICATE
+	db 25, DODUO
+	db 27, GRIMER
+	db 26, EEVEE
+	db 27, EEVEE
+	db 29, EEVEE
 	end_grass_wildmons
 
 	def_water_wildmons 0 ; encounter rate
