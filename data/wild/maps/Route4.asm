b38e3a4470cc95be6763f3b9a1cc03c2ac6634b5
seph-bit <seraphim.beatle@gmail.com>
2026-02-01 14:33:41 -0600

    Update Route4.asm

--- data/wild/maps/Route4.asm
+++ data/wild/maps/Route4.asm
@@ -1,15 +1,15 @@
 Route4WildMons:
 	def_grass_wildmons 20 ; encounter rate
 	db 10, RATTATA
-	db 10, SPEAROW
-	db  8, RATTATA
-	db  6, SANDSHREW
-	db  8, SPEAROW
-	db 10, SANDSHREW
-	db 12, RATTATA
 	db 12, SPEAROW
-	db  8, SANDSHREW
+	db 10, SANDSHREW
 	db 12, SANDSHREW
+	db 14, SPEAROW
+	db 14, SANDSHREW
+	db 12, RATTATA
+	db 12, PSYDUCK
+	db 14, PSYDUCK
+	db 15, PSYDUCK
 	end_grass_wildmons
 
 	def_water_wildmons 0 ; encounter rate
