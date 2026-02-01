0e713f28c1167602603159c223ae005c0240fb0b
seph-bit <seraphim.beatle@gmail.com>
2026-02-01 15:46:05 -0600

    Update DiglettsCave.asm

--- data/wild/maps/DiglettsCave.asm
+++ data/wild/maps/DiglettsCave.asm
@@ -1,15 +1,15 @@
 DiglettsCaveWildMons:
 	def_grass_wildmons 20 ; encounter rate
+	db 17, DIGLETT
 	db 18, DIGLETT
 	db 19, DIGLETT
-	db 17, DIGLETT
 	db 20, DIGLETT
-	db 16, DIGLETT
-	db 15, DIGLETT
 	db 21, DIGLETT
 	db 22, DIGLETT
-	db 29, DUGTRIO
-	db 31, DUGTRIO
+	db 27, ONIX
+	db 25, ONIX
+	db 25, DUGTRIO
+	db 26, DUGTRIO
 	end_grass_wildmons
 
 	def_water_wildmons 0 ; encounter rate
