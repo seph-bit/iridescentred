59a661571d8c4ddff7132c0a3cf4f358506e1547
seph-bit <seraphim.beatle@gmail.com>
2026-02-01 16:00:39 -0600

    Update Route8.asm

--- data/wild/maps/Route8.asm
+++ data/wild/maps/Route8.asm
@@ -1,28 +1,15 @@
 Route8WildMons:
 	def_grass_wildmons 15 ; encounter rate
-	db 18, PIDGEY
-IF DEF(_RED)
-	db 18, MANKEY
-	db 17, EKANS
-	db 16, GROWLITHE
-	db 20, PIDGEY
-	db 20, MANKEY
-	db 19, EKANS
-	db 17, GROWLITHE
-	db 15, GROWLITHE
-	db 18, GROWLITHE
-ENDC
-IF DEF(_BLUE)
-	db 18, MEOWTH
-	db 17, SANDSHREW
-	db 16, VULPIX
-	db 20, PIDGEY
-	db 20, MEOWTH
-	db 19, SANDSHREW
-	db 17, VULPIX
-	db 15, VULPIX
-	db 18, VULPIX
-ENDC
+	db 20, RATICATE
+	db 22, GROWLITHE
+	db 21, PRIMEAPE
+	db 23, GROWLITHE
+	db 23, RATICATE
+	db 22, PRIMEAPE
+	db 25, GROWLITHE
+	db 22, CUBONE
+	db 25, CUBONE
+	db 26, CUBONE
 	end_grass_wildmons
 
 	def_water_wildmons 0 ; encounter rate
