ac45e6862bc191b1f6470bc9bd50b6f482039f6f
seph-bit <seraphim.beatle@gmail.com>
2026-02-01 23:10:26 -0600

    Update SeafoamIslandsB2F.asm

--- data/wild/maps/SeafoamIslandsB2F.asm
+++ data/wild/maps/SeafoamIslandsB2F.asm
@@ -1,15 +1,15 @@
 SeafoamIslandsB2FWildMons:
 	def_grass_wildmons 10 ; encounter rate
 	db 37, GOLBAT
-	db 35, PSYDUCK
-	db 35, KRABBY
-	db 38, KRABBY
-	db 38, PSYDUCK
+	db 35, KINGLER
+	db 35, GOLDUCK
+	db 38, GOLBAT
+	db 38, GOLDUCK
 	db 39, GOLBAT
 	db 41, KINGLER
-	db 40, PSYDUCK
-	db 42, GOLDUCK
-	db 44, GOLDUCK
+	db 40, JYNX
+	db 42, JYNX
+	db 44, JYNX
 	end_grass_wildmons
 
 	def_water_wildmons 0 ; encounter rate
