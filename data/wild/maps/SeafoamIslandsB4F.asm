018a4e4dd12bb7f806a2a36daf3722a20019a629
seph-bit <seraphim.beatle@gmail.com>
2026-02-01 23:09:19 -0600

    Update SeafoamIslandsB4F.asm

--- data/wild/maps/SeafoamIslandsB4F.asm
+++ data/wild/maps/SeafoamIslandsB4F.asm
@@ -2,14 +2,14 @@ SeafoamIslandsB4FWildMons:
 	def_grass_wildmons 10 ; encounter rate
 	db 39, GOLBAT
 	db 41, STARYU
-	db 36, KRABBY
-	db 39, KRABBY
+	db 36, STARMIE
+	db 39, KINGLER
 	db 42, STARYU
 	db 41, GOLBAT
 	db 42, KINGLER
-	db 44, STARYU
-	db 43, STARMIE
-	db 46, STARMIE
+	db 44, JYNX
+	db 43, JYNX
+	db 46, JYNX
 	end_grass_wildmons
 
 	def_water_wildmons 0 ; encounter rate
