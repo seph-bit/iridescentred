8833281963c4c427922642d7af280c6ae8b0f962
seph-bit <seraphim.beatle@gmail.com>
2026-02-02 00:40:13 -0600

    Update CeruleanCave1F.asm

--- data/wild/maps/CeruleanCave1F.asm
+++ data/wild/maps/CeruleanCave1F.asm
@@ -1,20 +1,15 @@
 CeruleanCave1FWildMons:
 	def_grass_wildmons 10 ; encounter rate
-	db 46, GOLBAT
-	db 46, HYPNO
-	db 46, MAGNETON
-	db 49, DODRIO
-	db 49, VENOMOTH
-IF DEF(_RED)
-	db 52, ARBOK
-ENDC
-IF DEF(_BLUE)
-	db 52, SANDSLASH
-ENDC
-	db 49, KADABRA
-	db 52, PARASECT
-	db 53, RAICHU
-	db 53, DITTO
+	db 57, GOLBAT
+	db 58, PARASECT
+	db 62, MAGNETON
+	db 60, HYPNO
+	db 60, GOLBAT
+	db 61, PARASECT
+	db 63, HYPNO
+	db 64, ALAKAZAM
+	db 62, DITTO
+	db 65, DITTO
 	end_grass_wildmons
 
 	def_water_wildmons 0 ; encounter rate
