923374b3765e89fc6eae29790f02cc78e3a85a43
seph-bit <seraphim.beatle@gmail.com>
2026-02-02 00:42:38 -0600

    Update CeruleanCave2F.asm

--- data/wild/maps/CeruleanCave2F.asm
+++ data/wild/maps/CeruleanCave2F.asm
@@ -1,15 +1,15 @@
 CeruleanCave2FWildMons:
 	def_grass_wildmons 15 ; encounter rate
-	db 51, DODRIO
-	db 51, VENOMOTH
-	db 51, KADABRA
-	db 52, RHYDON
-	db 52, MAROWAK
-	db 52, ELECTRODE
-	db 56, CHANSEY
-	db 54, WIGGLYTUFF
-	db 55, DITTO
-	db 60, DITTO
+	db 60, DODRIO
+	db 61, MACHAMP
+	db 61, SLOWBRO
+	db 64, DODRIO
+	db 62, RAICHU
+	db 63, SLOWBRO
+	db 65, MACHAMP
+	db 63, GENGAR
+	db 64, GENGAR
+	db 66, GENGAR
 	end_grass_wildmons
 
 	def_water_wildmons 0 ; encounter rate
