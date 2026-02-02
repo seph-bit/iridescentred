8c006fabe75fc36544890a683c1ed44126c5b375
seph-bit <seraphim.beatle@gmail.com>
2026-02-01 23:37:38 -0600

    Update PokemonMansion1F.asm

--- data/wild/maps/PokemonMansion1F.asm
+++ data/wild/maps/PokemonMansion1F.asm
@@ -1,28 +1,15 @@
 PokemonMansion1FWildMons:
 	def_grass_wildmons 10 ; encounter rate
-IF DEF(_RED)
-	db 32, KOFFING
-	db 30, KOFFING
-	db 34, PONYTA
-	db 30, PONYTA
-	db 34, GROWLITHE
-	db 32, PONYTA
-	db 30, GRIMER
-	db 28, PONYTA
-	db 37, WEEZING
-	db 39, MUK
-ENDC
-IF DEF(_BLUE)
-	db 32, GRIMER
-	db 30, GRIMER
-	db 34, PONYTA
-	db 30, PONYTA
-	db 34, VULPIX
-	db 32, PONYTA
-	db 30, KOFFING
-	db 28, PONYTA
-	db 37, MUK
+	db 41, RATICATE
+	db 36, KOFFING
 	db 39, WEEZING
+	db 43, RATICATE
+	db 38, KOFFING
+	db 37, GRIMER
+	db 40, GRIMER
+	db 42, WEEZING
+	db 44, RAPIDASH
+	db 45, RAPIDASH
 ENDC
 	end_grass_wildmons
 
