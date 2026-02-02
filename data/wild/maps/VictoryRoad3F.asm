535d6d3dd2752784973ac9b35956cb4acc0315db
seph-bit <seraphim.beatle@gmail.com>
2026-02-02 00:36:44 -0600

    Update VictoryRoad3F.asm

--- data/wild/maps/VictoryRoad3F.asm
+++ data/wild/maps/VictoryRoad3F.asm
@@ -1,15 +1,15 @@
 VictoryRoad3FWildMons:
 	def_grass_wildmons 15 ; encounter rate
-	db 24, MACHOP
-	db 26, GEODUDE
-	db 22, ZUBAT
-	db 42, ONIX
-	db 40, VENOMOTH
-	db 45, ONIX
-	db 43, GRAVELER
-	db 41, GOLBAT
-	db 42, MACHOKE
-	db 45, MACHOKE
+	db 47, GOLBAT
+	db 47, KADABRA
+	db 47, HAUNTER
+	db 49, GOLBAT
+	db 51, GENGAR
+	db 53, ALAKAZAM
+	db 52, HITMONCHAN
+	db 53, GENGAR
+	db 51, KABUTOPS
+	db 54, KABUTOPS
 	end_grass_wildmons
 
 	def_water_wildmons 0 ; encounter rate
