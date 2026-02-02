5ff73a01b57fbfe72a3fccdb8b84d52e44d01b0f
seph-bit <seraphim.beatle@gmail.com>
2026-02-01 23:57:00 -0600

    Update VictoryRoad1F.asm

--- data/wild/maps/VictoryRoad1F.asm
+++ data/wild/maps/VictoryRoad1F.asm
@@ -1,15 +1,15 @@
 VictoryRoad1FWildMons:
 	def_grass_wildmons 15 ; encounter rate
-	db 24, MACHOP
-	db 26, GEODUDE
-	db 22, ZUBAT
-	db 36, ONIX
-	db 39, ONIX
-	db 42, ONIX
-	db 41, GRAVELER
-	db 41, GOLBAT
-	db 42, MACHOKE
-	db 43, MAROWAK
+	db 47, GOLBAT
+	db 48, GRAVELER
+	db 49, GOLBAT
+	db 45, ONIX
+	db 47, ONIX
+	db 48, GOLBAT
+	db 51, GOLEM
+	db 50, HITMONLEE
+	db 49, OMASTAR
+	db 52, OMASTAR
 	end_grass_wildmons
 
 	def_water_wildmons 0 ; encounter rate
