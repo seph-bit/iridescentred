37544bfc44b0d97bfe5d2a618265be27f8fe8147
seph-bit <seraphim.beatle@gmail.com>
2026-02-01 22:57:24 -0600

    Update SeafoamIslands1F.asm

--- data/wild/maps/SeafoamIslands1F.asm
+++ data/wild/maps/SeafoamIslands1F.asm
@@ -1,28 +1,15 @@
 SeafoamIslands1FWildMons:
 	def_grass_wildmons 15 ; encounter rate
-	db 30, SEEL
-IF DEF(_RED)
-	db 30, SLOWPOKE
-	db 30, SHELLDER
-	db 30, HORSEA
-	db 28, HORSEA
-	db 21, ZUBAT
-	db 29, GOLBAT
-	db 28, PSYDUCK
-	db 28, SHELLDER
-	db 38, GOLDUCK
-ENDC
-IF DEF(_BLUE)
-	db 30, PSYDUCK
-	db 30, STARYU
-	db 30, KRABBY
-	db 28, KRABBY
-	db 21, ZUBAT
-	db 29, GOLBAT
-	db 28, SLOWPOKE
-	db 28, STARYU
-	db 38, SLOWBRO
-ENDC
+	db 37, GOLBAT
+	db 35, SLOWPOKE
+	db 35, SEEL
+	db 37, SEEL
+	db 36, SLOWPOKE
+	db 38, GOLBAT
+	db 39, DEWGONG
+	db 38, SLOWPOKE
+	db 40, SLOWBRO
+	db 42, SLOWBRO
 	end_grass_wildmons
 
 	def_water_wildmons 0 ; encounter rate
