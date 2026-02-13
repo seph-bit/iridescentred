3ca6d35dcc238f1321cdb58275bdd3feb693aded
seph-bit <seraphim.beatle@gmail.com>
2026-02-12 22:13:03 -0600

    Update Route23.asm

--- data/wild/maps/Route23.asm
+++ data/wild/maps/Route23.asm
@@ -1,15 +1,15 @@
 Route23WildMons:
 	def_grass_wildmons 10 ; encounter rate
 	db 45, PRIMEAPE
-	db 43, DITTO
+	db 43, NIDORINO
 	db 44, FEAROW
-	db 45, DITTO
-	db 48, DITTO
+	db 45, EXEGGCUTE
+	db 48, NIDORINO
 	db 47, FEAROW
 	db 49, PRIMEAPE
-	db 46, CLOYSTER
+	db 46, NIDORINA
 	db 48, EXEGGUTOR
-	db 50, ARCANINE
+	db 50, EXEGGUTOR
 	end_grass_wildmons
 
 	def_water_wildmons 0 ; encounter rate
