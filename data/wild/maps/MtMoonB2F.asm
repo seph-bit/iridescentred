cf8624d343fdb0620444b06b6160821f0fe6f6e5
seph-bit <seraphim.beatle@gmail.com>
2026-02-12 20:42:47 -0600

    Update MtMoonB2F.asm

--- data/wild/maps/MtMoonB2F.asm
+++ data/wild/maps/MtMoonB2F.asm
@@ -5,8 +5,8 @@ MtMoonB2FWildMons:
 	db 12, ZUBAT
 	db 12, SANDSHREW
 	db 13, ZUBAT
-	db 11, MACHOP
-	db 14, MACHOP
+	db 11, PARAS
+	db 14, SANDSHREW
 	db 11, CLEFAIRY
 	db 12, CLEFAIRY
 	db 15, CLEFAIRY
