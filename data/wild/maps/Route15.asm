4b454d64acf42660fd8a544e77f6706e2dfe1c91
seph-bit <seraphim.beatle@gmail.com>
2026-02-12 21:08:37 -0600

    Update Route15.asm

--- data/wild/maps/Route15.asm
+++ data/wild/maps/Route15.asm
@@ -2,11 +2,11 @@ Route15WildMons:
 	def_grass_wildmons 15 ; encounter rate
 	db 30, PARASECT
 	db 29, BUTTERFREE
-	db 31, WEEPINBELL
+	db 31, RHYHORN
 	db 31, VENOMOTH
 	db 33, PARASECT
 	db 33, VENOMOTH
-	db 32, WEEPINBELL
+	db 32, RHYHORN
 	db 31, SCYTHER
 	db 33, SCYTHER
 	db 34, SCYTHER
