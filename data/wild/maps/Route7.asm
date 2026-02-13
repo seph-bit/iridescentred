26c9e13de24d9363c7d46febd6db5270c1decc91
seph-bit <seraphim.beatle@gmail.com>
2026-02-12 20:59:43 -0600

    Update Route7.asm

--- data/wild/maps/Route7.asm
+++ data/wild/maps/Route7.asm
@@ -1,12 +1,12 @@
 Route7WildMons:
 	def_grass_wildmons 15 ; encounter rate
 	db 24, DODUO
-	db 25, VULPIX
+	db 25, KOFFING
 	db 24, PRIMEAPE
 	db 25, DODUO
-	db 27, DODUO
+	db 27, KOFFING
 	db 26, PRIMEAPE
-	db 27, VULPIX
+	db 27, GRIMER
 	db 23, PONYTA
 	db 26, PONYTA
 	db 28, PONYTA
