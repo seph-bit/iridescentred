6d862bce648e76d69035d4d57da6074c95ec96b8
seph-bit <seraphim.beatle@gmail.com>
2026-02-12 21:07:37 -0600

    Update SeaRoutes.asm

--- data/wild/maps/SeaRoutes.asm
+++ data/wild/maps/SeaRoutes.asm
@@ -5,12 +5,12 @@ SeaRoutesWildMons:
 	def_water_wildmons 5 ; encounter rate
 	db 30, TENTACOOL
 	db 37, TENTACRUEL
-	db 30, DRATINI
+	db 30, MAGIKARP
 	db 32, TENTACOOL
 	db 32, TENTACOOL
 	db 36, GYARADOS
 	db 40, GYARADOS
 	db 39, TENTACRUEL
-	db 35, DRAGONAIR
-	db 38, DRAGONAIR
+	db 35, LAPRAS
+	db 38, LAPRAS
 	end_water_wildmons
