7847a8ab16acbda0c4afd98a04c0f509a342b6b4
seph-bit <seraphim.beatle@gmail.com>
2026-02-12 20:38:48 -0600

    Update Route22.asm

--- data/wild/maps/Route22.asm
+++ data/wild/maps/Route22.asm
@@ -1,13 +1,13 @@
 Route22WildMons:
 	def_grass_wildmons 25 ; encounter rate
-	db  3, POLIWAG
+	db  3, SPEAROW
 	db  3, NIDORAN_M
 	db  4, MANKEY
 	db  4, NIDORAN_M
 	db  6, POLIWAG
-	db  6, NIDORAN_M
+	db  6, SPEAROW
 	db  6, MANKEY
-	db  5, NIDORAN_F
+	db  5, POLIWAG
 	db  3, NIDORAN_F
 	db  4, NIDORAN_F
 	end_grass_wildmons
