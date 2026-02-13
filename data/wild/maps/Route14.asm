cb696edf26de0f04cff66122fe8f4344bcd36c32
seph-bit <seraphim.beatle@gmail.com>
2026-02-12 21:09:04 -0600

    Update Route14.asm

--- data/wild/maps/Route14.asm
+++ data/wild/maps/Route14.asm
@@ -1,13 +1,13 @@
 Route14WildMons:
 	def_grass_wildmons 15 ; encounter rate
-	db 29, GLOOM
+	db 29, RHYHORN
 	db 30, PIGDEOTTO
 	db 28, VENOMOTH
 	db 29, VENOMOTH
 	db 28, WEEPINBELL
 	db 32, VENOMOTH
 	db 30, WEEPINBELL
-	db 31, GLOOM
+	db 31, RHYHORN
 	db 31, PIDGEOT
 	db 32, PIDGEOT
 	end_grass_wildmons
