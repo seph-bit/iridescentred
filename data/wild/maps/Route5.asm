8011cf68d2c6fc1d82b91147f7d95500c69f005b
seph-bit <seraphim.beatle@gmail.com>
2026-02-12 20:47:48 -0600

    Update Route5.asm

--- data/wild/maps/Route5.asm
+++ data/wild/maps/Route5.asm
@@ -1,13 +1,13 @@
 Route5WildMons:
 	def_grass_wildmons 15 ; encounter rate
 IF DEF(_RED)
-	db 16, ABRA
+	db 16, PIDGEY
 	db 17, MEOWTH
 	db 19, MEOWTH
 	db 16, ODDISH
-	db 17, ABRA
+	db 17, PIDGEY
 	db 17, ODDISH
-	db 18, KADABRA
+	db 18, PIDGEOTTO
 	db 16, TANGELA
 	db 19, TANGELA
 	db 20, TANGELA
